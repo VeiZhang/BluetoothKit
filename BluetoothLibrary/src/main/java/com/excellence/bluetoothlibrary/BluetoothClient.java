6ae49cbe35def0c6a11068442c8cd9956b5c0003
master <690393541@qq.com>
2018-01-04 13:53:01 +0800

    Initial commit

---
+++ BluetoothLibrary/src/main/java/com/excellence/bluetoothlibrary/BluetoothClient.java
@@ -0,0 +1,14 @@
+package com.excellence.bluetoothlibrary;
+
+/**
+ * <pre>
+ *     author : VeiZhang
+ *     blog   : http://tiimor.cn
+ *     time   : 2017/12/29
+ *     desc   : 蓝牙工具类
+ * </pre>
+ */
+
+public class BluetoothClient
+{
+}
